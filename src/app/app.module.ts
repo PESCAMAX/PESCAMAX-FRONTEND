f5f8197b6caca0baecc545543a7f1e0fab5d86b4
Angelpdt <114520161+Angelpdt@users.noreply.github.com>
2024-05-08 22:32:24 -0500

    Estructura de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './Intranet/Intranet_components/menu-lateral/menu-lateral.component';
+import { ContenidoPrincipalComponent } from './Intranet/Intranet_components/contenido-principal/contenido-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    ContenidoPrincipalComponent
   ],
   imports: [
     BrowserModule,
