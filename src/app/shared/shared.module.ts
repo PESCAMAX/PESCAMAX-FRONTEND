10e867a5169b8bba9416f41a387230193449f553
Julian Cortes <mmm808383@gmail.com>
2024-05-29 17:41:23 -0500

    moleculas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { ButtonEnviarComponent } from './components/atoms/button-enviar/button-enviar.component';
 import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
 import { TablaEspecieComponent } from './components/molecules/tabla-especie/tabla-especie.component';
+import { CardAlertsComponent } from './components/molecules/card-alerts/card-alerts.component';
 
 @NgModule({
   declarations: [
     ButtonEnviarComponent,
     FlechaDespliegueComponent,
-    TablaEspecieComponent
+    TablaEspecieComponent,
+    CardAlertsComponent
   ],
   exports: [
     ButtonEnviarComponent,
     FlechaDespliegueComponent,
-    TablaEspecieComponent
+    TablaEspecieComponent,
+    CardAlertsComponent
   ]
 })
 export class SharedModule { }
