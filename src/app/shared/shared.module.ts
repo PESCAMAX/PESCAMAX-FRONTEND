f4bcc9071e39bc60083aa41c0b630082b6519edb
miguel Alarcon <luismiguelalarcon172@gmail.com>
2024-05-29 20:47:58 -0500

    subi el meni

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonEnviarComponent } from './components/atoms/button-enviar/button-enviar.component';
 import { FlechaDespliegueComponent } from './components/atoms/flecha-despliegue/flecha-despliegue.component';
 import { TablaEspecieComponent } from './components/molecules/tabla-especie/tabla-especie.component';
 import { CardAlertsComponent } from './components/molecules/card-alerts/card-alerts.component';
+import { MenuLateralComponent } from './components/molecules/menu-lateral/menu-lateral.component';
 
 @NgModule({
   declarations: [
     ButtonEnviarComponent,
     FlechaDespliegueComponent,
     TablaEspecieComponent,
-    CardAlertsComponent
+    CardAlertsComponent,
+    MenuLateralComponent
+  ],
+  imports: [
+    CommonModule
   ],
   exports: [
     ButtonEnviarComponent,
     FlechaDespliegueComponent,
     TablaEspecieComponent,
-    CardAlertsComponent
+    CardAlertsComponent,
+    MenuLateralComponent
   ]
 })
 export class SharedModule { }
