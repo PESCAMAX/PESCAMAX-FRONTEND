2fabdd6b11b555521c083013547977845ddd83df
Brandonpescador <git config --global user.name Tu>
2024-06-21 19:55:43 -0500

    modificar datos

--- src/app/features/monitoreo/services/api-form/api.service.ts
+++ src/app/features/monitoreo/services/api-form/api.service.ts
@@ -2,6 +2,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -23,10 +26,26 @@ export class ApiService {
     return this.http.delete(`${this.baseUrl}/CrearEspecie/Eliminar/${id}`);
   }
 
-  modificarEspecie(id: number, data: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}/CrearEspecie/Modificar/${id}`, data);
-  }
+  modificarEspecie(especie: Especie): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/CrearEspecie/Modificar`, especie)
+      .pipe(
+        catchError(this.handleError)
+      );
+    }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente o de red.
+      console.error('Ocurrió un error:', error.error.message);
+    } else {
+      // El backend retornó un código de respuesta sin éxito.
+      console.error(
+        `Backend retornó código ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Retorna un observable con un mensaje de error orientado al usuario
+    return throwError('Algo malo sucedió; por favor, inténtalo de nuevo más tarde.');
+  }
   listarMonitoreo(): Observable<any> {
     return this.http.get(`${this.baseUrl}/api/Monitoreo/Leer`);
   }
@@ -39,4 +58,15 @@ export interface Monitoreo {
   PH: Float32Array;
   FechaHora: Date;
   LoteID: number;
+}
+
+ export interface Especie {
+  id: number;
+  nombreEspecie: string;
+  tdsMinimo: number;
+  tdsMaximo: number;
+  temperaturaMinimo: number;
+  temperaturaMaximo: number;
+  phMinimo: number;
+  phMaximo: number;
 }
