f24eb9c682c5bf02ea90aaf3f79b5e8e2329d31f
Yulieth Viviana Trujillo Rodriguez <yulieth0218@gmail.com>
2024-06-27 15:14:31 -0500

    graficas

--- src/app/features/monitoreo/monitoreo.module.ts
+++ src/app/features/monitoreo/monitoreo.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { GraficaTdsComponent } from './pages/dashboard/grafica-tds/grafica-tds.component';
-
 
 @NgModule({
   declarations: [
-    GraficaTdsComponent
+
   ],
   imports: [
     CommonModule,
--- src/app/features/monitoreo/pages/dashboard/grafica-ph/grafica-ph.component.ts
+++ src/app/features/monitoreo/pages/dashboard/grafica-ph/grafica-ph.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+// grafica-ph.component.ts
+import { Component, OnInit } from '@angular/core';
+import * as pbi from 'powerbi-client';
 
 @Component({
   selector: 'app-grafica-ph',
   templateUrl: './grafica-ph.component.html',
-  styleUrl: './grafica-ph.component.css'
+  styleUrls: ['./grafica-ph.component.css']
 })
-export class GraficaPhComponent {
+export class GraficaPhComponent implements OnInit {
+  embedUrl: string = 'https://app.powerbi.com/reportEmbed?reportId=YOUR_REPORT_ID'; // Reemplaza con tu embed URL
+  accessToken: string = 'YOUR_ACCESS_TOKEN'; // Reemplaza con tu access token
 
+  constructor() {}
+
+  ngOnInit() {
+    this.embedPowerBIReport();
+  }
+
+  embedPowerBIReport() {
+    const embedConfiguration: pbi.IEmbedConfiguration = {
+      type: 'report',
+      id: 'YOUR_REPORT_ID', // Reemplaza con el ID de tu informe
+      embedUrl: this.embedUrl,
+      accessToken: this.accessToken,
+      tokenType: pbi.models.TokenType.Embed,
+      settings: {
+        filterPaneEnabled: false,
+        navContentPaneEnabled: false
+      }
+    };
+
+    const reportContainer = document.getElementById('reportContainer');
+    if (reportContainer) {
+      const powerbiService = new pbi.service.Service(
+        pbi.factories.hpmFactory,
+        pbi.factories.wpmpFactory,
+        pbi.factories.routerFactory
+      );
+
+      powerbiService.embed(reportContainer, embedConfiguration);
+    } else {
+      console.error('No se pudo encontrar el contenedor del informe');
+    }
+  }
 }
+
