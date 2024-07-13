e53793d74c2821bfe6b2ad12fd92c951fe1641c5
luis-perez <bermudezluisperezangel@gmail.com>
2024-07-13 11:19:49 -0500

    temperatura

--- src/app/shared/components/molecules/datapicker/datapicker.component.ts
+++ src/app/shared/components/molecules/datapicker/datapicker.component.ts
@@ -31,3 +31,41 @@ export class DatapickerComponent implements OnInit {
     });
   }
 }
+
+
+
+
+
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { FormGroup, FormBuilder } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-datapicker',
+//   templateUrl: './datapicker.component.html',
+//   styleUrls: ['./datapicker.component.css']
+// })
+// export class DatapickerComponent implements OnInit {
+//   @Input() minDate!: Date;
+//   @Input() maxDate!: Date;
+//   @Output() dateRangeSelected = new EventEmitter<{startDate: Date, endDate: Date}>();
+
+//   dateRange!: FormGroup;
+
+//   constructor(private fb: FormBuilder) {}
+
+//   ngOnInit() {
+//     this.dateRange = this.fb.group({
+//       startDate: [''],
+//       endDate: ['']
+//     });
+
+//     this.dateRange.valueChanges.subscribe(val => {
+//       if (val.startDate && val.endDate) {
+//         this.dateRangeSelected.emit({
+//           startDate: val.startDate,
+//           endDate: val.endDate
+//         });
+//       }
+//     });
+//   }
+// }
