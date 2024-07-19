0f0b1d0a6aac8e93184934e8279242ce9f0fe6c3
aluismiguel <luismiguelalarcon172@gmail.com>
2024-07-18 20:46:23 -0500

    boton responsil y crear especie ok

--- src/app/features/monitoreo/services/menu-state/menu-state.service.ts
+++ src/app/features/monitoreo/services/menu-state/menu-state.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MenuStateService {
+  [x: string]: any;
   private subMenuVisible: number | null = null;
 
   constructor() { }
