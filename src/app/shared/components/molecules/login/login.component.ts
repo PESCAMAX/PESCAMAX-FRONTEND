42d75afb39e2535b4eed9e5f372fcf9d0e2cd470
“FAFNIR92” <ramirezgiraldosebastian30@gmail.com>
2024-07-22 19:30:11 -0500

    menu-lateral terminado

--- src/app/shared/components/molecules/login/login.component.ts
+++ src/app/shared/components/molecules/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../../features/monitoreo/services/api-login/auth.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -17,12 +16,17 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
+    // Limpiar mensajes anteriores
+    this.successMessage = '';
+    this.errorMessage = '';
+
     const user = { username: this.username, password: this.password };
     this.authService.login(user).subscribe(
       (response) => {
         if (response.success) {
           this.successMessage = response.message || 'Login successful!';
           setTimeout(() => {
+            this.successMessage = ''; // Limpiar el mensaje de éxito
             this.router.navigate(['/crear-especie']);
           }, 2000);
         } else {
