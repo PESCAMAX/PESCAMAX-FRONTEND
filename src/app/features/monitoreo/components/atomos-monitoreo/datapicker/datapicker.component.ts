a00deec20d45418f46bc6bdba101d49701bc4d9b
yulieth <Yulieth02218@gmail.com>
2024-08-03 15:52:13 -0500

    fff

--- src/app/features/monitoreo/components/atomos-monitoreo/datapicker/datapicker.component.ts
+++ src/app/features/monitoreo/components/atomos-monitoreo/datapicker/datapicker.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatDateRangePicker } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-datapicker',
   templateUrl: './datapicker.component.html',
   styleUrls: ['./datapicker.component.css']
 })
-export class DatapickerComponent implements OnInit {
+export class DatapickerComponent implements OnInit, AfterViewInit {
   @Input() minDate!: Date;
-  @Input() maxDate: Date = new Date(); // Por defecto, la fecha actual
-  @Output() dateRangeSelected = new EventEmitter<{startDate: Date, endDate: Date}>();
+  @Input() maxDate: Date = new Date();
+  @Output() dateRangeSelected = new EventEmitter<{ startDate: Date, endDate: Date }>();
 
   dateRange!: FormGroup;
+  @ViewChild('picker') picker!: MatDateRangePicker<Date>;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.dateRange = this.fb.group({
-      startDate: [''],
-      endDate: ['']
+      startDate: [null],
+      endDate: [null]
     });
 
     this.dateRange.valueChanges.subscribe(val => {
@@ -31,10 +33,16 @@ export class DatapickerComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    // Abre el picker automáticamente después de que la vista se inicializa
+    setTimeout(() => {
+      if (this.picker) {
+        this.picker.open();
+      }
+    });
+  }
 
-  // Método para deshabilitar fechas
   disableDates = (date: Date): boolean => {
     return date > this.maxDate || date < this.minDate;
   }
 }
-
