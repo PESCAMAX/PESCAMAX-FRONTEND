3c87e20ae672d2c9611d8c752ce3b69880d007c4
yulieth <Yulieth02218@gmail.com>
2024-08-08 13:52:58 -0500

    ya sirve

--- src/app/features/monitoreo/components/moleculas-monitoreo/tabla-seleccionar/tabla-seleccionar.component.ts
+++ src/app/features/monitoreo/components/moleculas-monitoreo/tabla-seleccionar/tabla-seleccionar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Alerta, ApiService, Especie, Monitoreo, EspecieLoteDTO } from '../../../services/api-form/api.service';import { AlertService } from '../../../services/api-alert/alert.service';
+import { Alerta, ApiService, Especie, Monitoreo, EspecieLoteDTO } from '../../../services/api-form/api.service';
+import { AlertService } from '../../../services/api-alert/alert.service';
 import { AuthService } from '../../../../../core/services/api-login/auth.service';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-tabla-seleccionar',
   templateUrl: './tabla-seleccionar.component.html',
@@ -17,7 +19,7 @@ export class TablaSeleccionarComponent implements OnInit {
   especiePorLote: { [loteId: number]: number } = {};
 
   constructor(
-    private apiService: ApiService, 
+    private apiService: ApiService,
     private alertService: AlertService,
     private authService: AuthService,
     private route: ActivatedRoute
@@ -25,14 +27,12 @@ export class TablaSeleccionarComponent implements OnInit {
     this.userId = this.authService.getUserId();
   }
 
-
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.userId = params['userId'] || this.userId;
       this.loadData();
       this.cargarEspeciePorLote();
     });
-    
   }
 
   onMenuToggle(isOpen: boolean) {
@@ -50,6 +50,7 @@ export class TablaSeleccionarComponent implements OnInit {
       error: (error) => console.error('Error al cargar especies por lote:', error)
     });
   }
+
   loadData() {
     // Load especies
     this.apiService.listarEspecies(this.userId).subscribe({
@@ -59,34 +60,17 @@ export class TablaSeleccionarComponent implements OnInit {
       },
       error: (error) => console.error('Error al cargar especies:', error)
     });
-    
   }
+
   loadMonitoreo() {
     this.apiService.listarMonitoreo(this.userId).subscribe({
       next: (response) => {
-        console.log('Respuesta completa:', response);
         if (response && response.response) {
           this.lotes = response.response;
           this.uniqueLotes = [...new Set(this.lotes.map(lote => lote.LoteID))];
-          console.log('Lotes únicos:', this.uniqueLotes);
         } else {
           console.error('La respuesta no tiene el formato esperado');
         }
-      },
-      error: (error) => {
-        console.error('Error al listar lotes:', error);
-      }
-    });
-    
-
-    this.apiService.listarMonitoreo(this.userId).subscribe({
-      // ...
-    
-      next: (response) => {
-        console.log('Lotes:', response);
-        this.lotes = response.response;
-        this.uniqueLotes = [...new Set(this.lotes.map(lote => lote.LoteID))];
-        console.log('Lotes únicos:', this.uniqueLotes);
       },
       error: (error) => {
         console.error('Error al listar lotes:', error);
@@ -102,15 +86,42 @@ export class TablaSeleccionarComponent implements OnInit {
 
   guardarSeleccion(especieId: number): void {
     const loteId = this.selectedLote[especieId];
-  
+
     if (!loteId) {
       this.alertService.showAlert('warning', 'Selección incompleta', 'Seleccione un lote para la especie.');
       return;
     }
-  
+
+    const especie = this.especies.find(e => e.Id === especieId);
+    const lotesFiltrados = this.lotes.filter(l => l.LoteID === loteId);
+    const ultimoLote = lotesFiltrados.reduce((prev, current) =>
+      (new Date(prev.FechaHora) > new Date(current.FechaHora)) ? prev : current
+    );
+
+    if (!especie) {
+      console.error('No se encontró la especie correspondiente con ID:', especieId);
+      this.alertService.showAlert('danger', 'Error de datos', 'No se encontró la especie correspondiente.');
+      return;
+    }
+
+    if (!ultimoLote) {
+      console.error('No se encontró el lote correspondiente con ID:', loteId);
+      console.log('Lotes disponibles:', this.lotes);
+      this.alertService.showAlert('danger', 'Error de datos', 'No se encontró el lote correspondiente.');
+      return;
+    }
+
+    const problemas = this.obtenerProblemas(especie, ultimoLote);
+
     this.apiService.asignarEspecieALote(especieId, loteId, this.userId).subscribe({
       next: () => {
-        this.alertService.showAlert('info', 'Asignación guardada', 'La especie ha sido asignada al lote correctamente.');
+        if (problemas.length > 0) {
+          console.log('Problemas detectados:', problemas.join(', '));
+          this.crearAlerta(especie, loteId, problemas);
+          this.alertService.showAlert('danger', 'Parámetros fuera del rango seguro', problemas.join(', '));
+        } else {
+          this.alertService.showAlert('info', 'Asignación guardada', 'La especie ha sido asignada al lote correctamente.');
+        }
         this.cargarEspeciePorLote();
       },
       error: (error) => {
@@ -120,67 +131,36 @@ export class TablaSeleccionarComponent implements OnInit {
     });
   }
 
-  
-  verificarValores(especieId: number, loteId: number): void {
-    console.log('Verificando valores para especie ID:', especieId, 'y lote ID:', loteId);
-    const especie = this.especies.find(e => e.Id === especieId);
-    const lotesFiltrados = this.lotes.filter(l => l.LoteID === loteId);
-    const ultimoLote = lotesFiltrados.reduce((prev, current) => 
-      (new Date(prev.FechaHora) > new Date(current.FechaHora)) ? prev : current
-    );
-  
-    if (!especie) {
-      console.error('No se encontró la especie correspondiente con ID:', especieId);
-      this.alertService.showAlert('danger', 'Error de datos', 'No se encontró la especie correspondiente.');
-      return;
-    }
-  
-    if (!ultimoLote) {
-      console.error('No se encontró el lote correspondiente con ID:', loteId);
-      console.log('Lotes disponibles:', this.lotes);
-      this.alertService.showAlert('danger', 'Error de datos', 'No se encontró el lote correspondiente.');
-      return;
-    }
-  
-    const problemas = this.obtenerProblemas(especie, ultimoLote);
-
-    if (problemas.length > 0) {
-      console.log('Problemas detectados:', problemas.join(', '));
-      this.alertService.showAlert('danger', 'Parámetros fuera del rango seguro', problemas.join(', '));
-      
-      // Crear alerta
-      const alerta: Alerta = {
-        EspecieID: especie.Id,
-        Nombre: especie.NombreEspecie,
-        LoteID: loteId,
-        Descripcion: problemas.join(', '),
-        UserId: this.userId  // Añadir el userId aquí
-      };
-      this.apiService.crearAlerta(alerta).subscribe({
-        next: (response) => console.log('Alerta creada:', response),
-        error: (error) => console.error('Error al crear alerta:', error)
-      });
-    } else {
-      console.log('Todos los parámetros están dentro del rango seguro.');
-      this.alertService.showAlert('info', 'Validación exitosa', 'Todos los parámetros están dentro del rango seguro.');
-    }
-  }
- 
   obtenerProblemas(especie: Especie, lote: Monitoreo): string[] {
     const problemas = [];
-  
+
     if (lote.tds < especie.TdsMinimo || lote.tds > especie.TdsMaximo) {
       problemas.push(`TDS está fuera del rango seguro (${especie.TdsMinimo}-${especie.TdsMaximo}). Actual: ${lote.tds}`);
     }
-  
+
     if (lote.Temperatura < especie.TemperaturaMinimo || lote.Temperatura > especie.TemperaturaMaximo) {
       problemas.push(`Temperatura está fuera del rango seguro (${especie.TemperaturaMinimo}-${especie.TemperaturaMaximo}). Actual: ${lote.Temperatura}`);
     }
-  
+
     if (lote.PH < especie.PhMinimo || lote.PH > especie.PhMaximo) {
       problemas.push(`pH está fuera del rango seguro (${especie.PhMinimo}-${especie.PhMaximo}). Actual: ${lote.PH}`);
     }
-  
+
     return problemas;
+  }
+
+  crearAlerta(especie: Especie, loteId: number, problemas: string[]): void {
+    const alerta: Alerta = {
+      EspecieID: especie.Id,
+      Nombre: especie.NombreEspecie,
+      LoteID: loteId,
+      Descripcion: problemas.join(', '),
+      UserId: this.userId
+    };
+
+    this.apiService.crearAlerta(alerta).subscribe({
+      next: (response) => console.log('Alerta creada:', response),
+      error: (error) => console.error('Error al crear alerta:', error)
+    });
   }
 }
