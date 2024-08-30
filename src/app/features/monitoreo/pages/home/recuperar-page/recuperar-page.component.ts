18e2fa961d5dd004c58580d326c2dd2124ed52d1
PrincessNight10 <santiagorojas772@gmail.com>
2024-08-29 21:00:40 -0500

    cambioslogins

--- src/app/features/monitoreo/pages/home/recuperar-page/recuperar-page.component.ts
+++ src/app/features/monitoreo/pages/home/recuperar-page/recuperar-page.component.ts
@@ -1,55 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recuperar-page',
   templateUrl: './recuperar-page.component.html',
   styleUrls: ['./recuperar-page.component.css']
 })
-export class RecuperarPageComponent implements OnInit {
-  ngOnInit() {
-    this.showFirstImage();
-    this.startSlideshow();
-  }
-
-  showFirstImage() {
-    const images = [
-      '.../../../../../../assets/images/contra1.png',
-      '../../../../../../assets/images/contra4.png ',
-      '.../../../../../../assets/images/contra3.png'
-    ];
-
-    const backgroundElement = document.getElementById('background');
-
-    // Mostrar la primera imagen inmediatamente
-    backgroundElement!.style.backgroundImage = `url(${images[0]})`;
-  }
-
-  startSlideshow() {
-    const images = [
-   '.../../../../../../assets/images/contra1.png',
-      '../../../../../../assets/images/contra4.png ',
-      '.../../../../../../assets/images/contra3.png'
-    ];
-
-    let currentIndex = 1; // Empezamos desde la segunda imagen
-    const backgroundElement = document.getElementById('background');
-
-    setInterval(() => {
-      // Aplicar el desenfoque antes del cambio de imagen
-      backgroundElement!.classList.add('blur');
-      
-      setTimeout(() => {
-        // Cambiar la imagen de fondo
-        backgroundElement!.style.backgroundImage = `url(${images[currentIndex]})`;
-        currentIndex = (currentIndex + 1) % images.length;
-
-        // Esperar un momento y luego eliminar el desenfoque
-        setTimeout(() => {
-          backgroundElement!.classList.remove('blur');
-        }, 1000); // Tiempo igual al de la transición del desenfoque
-
-      }, 1000); // Tiempo de desenfoque antes de cambiar la imagen
-
-    }, 6000); // Cambiar imagen cada 6 segundos (incluyendo tiempo de transición)
-  }
+export class RecuperarPageComponent {
+  // El componente está vacío y no tiene lógica en este momento.
 }
