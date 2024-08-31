16a0c6607e777ba963aa5fe5f84879431f6585b1
PrincessNight10 <santiagorojas772@gmail.com>
2024-08-31 16:44:36 -0500

    cambioslogin

--- src/app/features/monitoreo/components/atomos-monitoreo/button-cancelar/button-cancelar.component.ts
+++ src/app/features/monitoreo/components/atomos-monitoreo/button-cancelar/button-cancelar.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ButtonCancelarComponent {
   @Input() texto: string = 'Cancelar';
-  @Input() estilo: 'blanco' | 'rojo' = 'blanco';
+  @Input() estilo: 'blanco' | 'rojo' | 'azul' = 'blanco'; // Añadido 'azul'
   @Input() customClass: string = '';
   @Output() cancelar = new EventEmitter<void>();
 
@@ -16,11 +16,37 @@ export class ButtonCancelarComponent {
   }
 
   get buttonClasses(): string {
-    const baseClasses = 'font-bold py-2 px-4 rounded focus:outline-none';
+    const baseClasses = 'font-bold py-2 px-4 rounded focus:outline-none transition-all duration-300';
     if (this.estilo === 'rojo') {
       return `${baseClasses} bg-red-500 hover:bg-red-700 text-white ${this.customClass}`;
+    } else if (this.estilo === 'azul') {
+      return `${baseClasses} text-blue-600 border border-blue-600 hover:border-blue-700 hover:text-blue-700 ${this.customClass}`;
     } else {
       return `${baseClasses} bg-white text-gray-700 border border-gray-300 hover:bg-gray-50 ${this.customClass}`;
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
