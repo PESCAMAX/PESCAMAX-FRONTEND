f51c7088bcc6b94317c9ba07764d220bfb6d413d
Angelpdt <114520161+Angelpdt@users.noreply.github.com>
2024-09-13 00:48:02 -0500

    a veces me salen unos errores que ni la IA entiende pero bueno

--- src/app/features/monitoreo/components/atomos-monitoreo/cards-info/cards-info.component.ts
+++ src/app/features/monitoreo/components/atomos-monitoreo/cards-info/cards-info.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import { ClimaService } from '../../../services/clima/clima.service';
+import { AuthService } from '../../../../../core/services/api-login/auth.service';
 
 @Component({
   selector: 'app-cards-info',
   templateUrl: './cards-info.component.html',
   styleUrls: ['./cards-info.component.css']
 })
-export class CardsInfoComponent implements OnChanges {
+export class CardsInfoComponent implements OnChanges, OnInit {
   @Input() title: string = '';
   @Input() value: number | string = '';
   @Input() trend: 'up' | 'down' | 'none' = 'none';
@@ -17,15 +19,48 @@ export class CardsInfoComponent implements OnChanges {
   @Input() link: string = ''; // Nueva propiedad para el enlace
   @Input() disableLink: boolean = false; // Nueva propiedad para deshabilitar el enlace
   @Input() nombreCiudad: string = ''; // Nueva propiedad para el nombre de la ciudad
-  @Input() temperaturaActual: number = 0; // Nueva propiedad para la temperatura actual
   @Input() estadoMeteoro: string = ''; // Nueva propiedad para el estado meteorológico
-
+  climaData: any;
   previousValue: number | string = '';
+
+  constructor(private climaService: ClimaService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.title === 'Clima') {
+      this.cargarUltimoClima();
+    }
+  }
+
+  cargarUltimoClima(): void {
+    const userId = this.authService.getUserId();
+    this.climaService.obtenerUltimoClimaPorUsuario(userId).subscribe(
+      data => {
+        this.climaData = data;
+        this.value = this.climaData?.TemperaturaActual?.toFixed(2) || 0; // Actualizar el valor principal con dos decimales
+        console.log('Último clima data:', this.climaData);
+      },
+      error => {
+        console.error('Error al cargar el último clima:', error);
+      }
+    );
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['value']) {
       this.previousValue = changes['value'].previousValue;
     }
+  }
+
+  get temperaturaActual(): number {
+    return this.climaData?.TemperaturaActual || 0;
+  }
+
+  get humedad(): number {
+    return this.climaData?.Humedad || 0;
+  }
+
+  get nubes(): number {
+    return this.climaData?.Nubes || 0;
   }
 
   get cardColor(): string {
